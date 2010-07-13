d0d4d0f54c1e5a6f4959a23a0b016ffe5614b788
Gregory Crosswhite <gcross@phys.washington.edu>
2010-07-12 20:52:22 -0700

    Added notes with the old and new combinatorics forumal.

---
+++ CodeLattice.mm
@@ -0,0 +1,14 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="CodeLattice" ID="ID_1663122908" CREATED="1269638164911" MODIFIED="1269638205422">
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="Number of ray labelings" POSITION="right" ID="ID_111477891" CREATED="1269638194983" MODIFIED="1278992997153">
+<node TEXT="Old formula (wrong)" ID="ID_833632011" CREATED="1278993000741" MODIFIED="1278993012982">
+<hook EQUATION="3^{n-2}\cdot 2" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="Correct formula" ID="ID_15826017" CREATED="1278993039158" MODIFIED="1278993047993">
+<hook EQUATION="3^{n-1}/2" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+</node>
+</node>
+</map>
