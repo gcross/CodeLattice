94d616a66b9cdb6fbd0a06ffec6005dc422eba35
Gregory Crosswhite <gcross@phys.washington.edu>
2010-07-22 14:19:16 -0700

    Updated formulas in the mind-map notes.

--- CodeLattice.mm
+++ CodeLattice.mm
@@ -3,11 +3,14 @@
 <node TEXT="CodeLattice" ID="ID_1663122908" CREATED="1269638164911" MODIFIED="1269638205422">
 <hook NAME="MapStyle" max_node_width="600"/>
 <node TEXT="Number of ray labelings" POSITION="right" ID="ID_111477891" CREATED="1269638194983" MODIFIED="1278992997153">
-<node TEXT="Old formula (wrong)" ID="ID_833632011" CREATED="1278993000741" MODIFIED="1278993012982">
+<node TEXT="First wrong attempt" ID="ID_833632011" CREATED="1278993000741" MODIFIED="1279057151953">
 <hook EQUATION="3^{n-2}\cdot 2" NAME="plugins/latex/LatexNodeHook.properties"/>
 </node>
-<node TEXT="Correct formula" ID="ID_15826017" CREATED="1278993039158" MODIFIED="1278993047993">
+<node TEXT="Second wrong attempt" ID="ID_15826017" CREATED="1278993039158" MODIFIED="1279057156401">
 <hook EQUATION="3^{n-1}/2" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="Correct formula" ID="ID_196453321" CREATED="1279057140982" MODIFIED="1279057196433">
+<hook EQUATION="1+\frac{3^{n-1}-1}{2}" NAME="plugins/latex/LatexNodeHook.properties"/>
 </node>
 </node>
 </node>
