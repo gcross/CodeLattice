5e6df55ee5b9dece4586c392ef66efcef5dc8306
Gregory Crosswhite <gcross@phys.washington.edu>
2010-08-09 15:25:59 -0700

    Reworked code to use the diagrams instead of generating postscript.

--- scripts/plot-tiling-results.hs
+++ scripts/plot-tiling-results.hs
@@ -1,16 +1,16 @@
 -- @+leo-ver=4-thin
--- @+node:gcross.20100808143551.1695:@thin plot-tiling-results.hs
+-- @+node:gcross.20100809113206.1715:@thin plot-tiling-results.hs
 -- @@language Haskell
 
 -- @<< Language extensions >>
--- @+node:gcross.20100808143551.1696:<< Language extensions >>
+-- @+node:gcross.20100809113206.1716:<< Language extensions >>
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UnicodeSyntax #-}
--- @-node:gcross.20100808143551.1696:<< Language extensions >>
+-- @-node:gcross.20100809113206.1716:<< Language extensions >>
 -- @nl
 
 -- @<< Import needed modules >>
--- @+node:gcross.20100808143551.1697:<< Import needed modules >>
+-- @+node:gcross.20100809113206.1717:<< Import needed modules >>
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans.Writer.Lazy
@@ -23,31 +23,31 @@ import Data.Tuple.Select
 import Database.Enumerator
 import Database.PostgreSQL.Enumerator
 
+import Graphics.Rendering.Diagrams as D
+
 import System.Environment
 import System.Exit
 import System.IO
 
-import Text.PrettyPrint.HughesPJ
 import Text.Printf
 import Text.Read
 
 import CodeLattice.Database
 import CodeLattice.Tilings
--- @-node:gcross.20100808143551.1697:<< Import needed modules >>
+-- @-node:gcross.20100809113206.1717:<< Import needed modules >>
 -- @nl
 
 -- @+others
--- @+node:gcross.20100808143551.1698:Functions
--- @+node:gcross.20100808143551.1699:getArguments
-getArguments :: IO (Tiling,Handle)
+-- @+node:gcross.20100809113206.1718:Functions
+-- @+node:gcross.20100809113206.1719:getArguments
+getArguments :: IO (Tiling,String)
 getArguments = do
     args ← getArgs
-    (tiling_name,maybe_filename) ←
+    (tiling_name,filename) ←
         case args of
-            [x] → return (x,Nothing)
-            [x,y] → return (x,Just y)
+            [x,y] → return (x,y)
             _ → do
-                putStrLn "Usage:  plot-tiling-results <tiling> [output filename]"
+                putStrLn "Usage:  plot-tiling-results <tiling> <output filename.pdf>"
                 exitFailure
     tiling ←
         case find ((== tiling_name) . tilingName) tilings of
@@ -56,47 +56,25 @@ getArguments = do
                 putStrLn "Tiling must be one of the following:"
                 mapM_ (putStrLn . ('\t':) . tilingName) tilings -- '
                 exitFailure
-    handle ← maybe (return stdout) (flip openFile WriteMode) maybe_filename
-    return (tiling,handle)
--- @-node:gcross.20100808143551.1699:getArguments
--- @+node:gcross.20100809113206.1679:drawPolyAt
-drawPolyAt :: Double → Int → Double → Double → Doc
-drawPolyAt base_radius n cx cy = hsep
-    [text "gsave newpath"
-    ,first_point <+> text "moveto"
-    ,hsep [point <+> text "lineto" | point ← rest_points]
-    ,text "closepath stroke grestore"
-    ]
+    return (tiling,filename)
+-- @-node:gcross.20100809113206.1719:getArguments
+-- @+node:gcross.20100809113206.1720:drawPolyAt
+drawPolyAt :: Double → Int → Double → Double → Diagram
+drawPolyAt base_radius n cx cy =
+    position . (:[]) . ((,) (cx,cy)) $ rotRegPoly n (scaling*base_radius) rotation
   where
-    (first_point:rest_points) =
-        [let angle = (rotation_in_degrees + fromIntegral i * polygon_angle)/180*pi
-             x = cx + radius * cos angle
-             y = cy + radius * sin angle
-         in double x <+> double y
-        | i ← [0..n-1]
-        ]
+    rotation = rotation_in_degrees / 360
     radius = scaling * base_radius
     (scaling,rotation_in_degrees) = (!! (n-3))
         [(1.0,90)
-        ,(1.2,45)
-        ,(1.4,-90)
-        ,(1.6,0)
+        ,(1.3,45)
+        ,(1.2,-90)
+        ,(1.3,0)
         ]
-    polygon_angle = fromIntegral (360 `div` n)
--- @-node:gcross.20100809113206.1679:drawPolyAt
--- @+node:gcross.20100809113206.1688:setPageSize
-setPageSize :: Int → Int → Doc
-setPageSize width height = hsep
-    [text "<< /PageSize ["
-    ,int width
-    ,int height
-    ,text "] >> setpagedevice"
-    ]
-
--- @-node:gcross.20100809113206.1688:setPageSize
--- @+node:gcross.20100808143551.1700:main
+-- @-node:gcross.20100809113206.1720:drawPolyAt
+-- @+node:gcross.20100809113206.1722:main
 main = do
-    (tiling@Tiling{..},handle) ← getArguments
+    (tiling@Tiling{..},filename) ← getArguments
     connection ← makeConnection "reader"
     plotlines ←
         fmap (
@@ -114,16 +92,9 @@ main = do
             fetch4
             ([] :: [(Int,Int,Int,Int)])
             "Error fetching results from the database:"
-    hPutStrLn handle . renderStyle (style { mode = LeftMode }) . vcat $
-        [text "%!PS-Adobe-3.0"
-        ,setPageSize 400 400
-        ,vcat [drawPolyAt 100 i 200 200 | i <- [3..6]]
-        ,text "showpage"
-        ]
-    hFlush handle
-    hClose handle
--- @-node:gcross.20100808143551.1700:main
--- @-node:gcross.20100808143551.1698:Functions
+    renderAs PDF filename Auto . D.union $ [drawPolyAt 100 n 0 0 | n <- [3..6]]
+-- @-node:gcross.20100809113206.1722:main
+-- @-node:gcross.20100809113206.1718:Functions
 -- @-others
--- @-node:gcross.20100808143551.1695:@thin plot-tiling-results.hs
+-- @-node:gcross.20100809113206.1715:@thin plot-tiling-results.hs
 -- @-leo
